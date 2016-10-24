3e2e9913815b977d3e88a2e534e48ad5e4b7571f
EAR Patrick <patrick-hk@outlook.com>
2016-10-24 11:52:19 +0200

    [D1] End the step 3

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import template from './app.component.html';
+
+import {Parties} from '../../../both/collections/parties.collection';
+import {Observable} from 'rxjs/Observable';
+
+import { Party } from '../../../both/models/party.model';
 
 @Component({
     selector: 'app',
     template
 })
-export class AppComponent {}
+
+export class AppComponent {
+    parties: Observable<Party[]>;
+
+    constructor() {
+        this.parties = Parties.find({}).zone();
+    }
+}
