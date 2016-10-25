99948cb8e1b9025cb1e70eb24078900fb11a98d6
EAR Patrick <patrick-hk@outlook.com>
2016-10-25 10:47:47 +0200

    [ROUTING] Added routing to project

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,20 +2,9 @@ import {Component} from '@angular/core';
 
 import template from './app.component.html';
 
-import {Parties} from '../../../both/collections/parties.collection';
-import {Observable} from 'rxjs/Observable';
-
-import { Party } from '../../../both/models/party.model';
-
 @Component({
     selector: 'app',
     template
 })
 
-export class AppComponent {
-    parties: Observable<Party[]>;
-
-    constructor() {
-        this.parties = Parties.find({}).zone();
-    }
-}
+export class AppComponent {}
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import {AppComponent} from "./app.component";
+import { PARTIES_DECLARATIONS } from './parties';
+import { routes } from './app.routes';
+
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes)
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        ...PARTIES_DECLARATIONS
     ],
     bootstrap: [
         AppComponent
     ]
+
 })
 export class AppModule {}
